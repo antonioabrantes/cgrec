3bf94b9f8f45e2f132ca3274b9ad20ca17299c51
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-10-05 21:16:08 -0300

    Update main_embedding.py

--- main_embedding.py
+++ main_embedding.py
@@ -261,27 +261,27 @@ class chat_gen():
                 ##content = bytes(content, "utf-8").decode("unicode_escape")
     
                 # Replace escaped newlines with actual newlines
-                content = re.sub(r'\\n', '\n', content)
+                ##content = re.sub(r'\\n', '\n', content)
                 # Remove special tokens
                 ##content = re.sub(r'\s*<EOS>\s*<pad>\s*', ' ', content)
                 # Remove any remaining multiple spaces
-                content = re.sub(r'\s+', ' ', content).strip()
+                ##content = re.sub(r'\s+', ' ', content).strip()
     
                 # Decode HTML entities
                 ##content = html.unescape(content)
     
                 # Replace incorrect unicode characters with correct ones
-                content = content.encode('latin1').decode('utf-8', 'ignore')
+                ##content = content.encode('latin1').decode('utf-8', 'ignore')
     
                 # Remove or replace special characters and mathematical symbols
                 # This step may need to be customized based on the specific symbols in your documents
-                content = re.sub(r'â', '-', content)
-                content = re.sub(r'â', '∈', content)
-                content = re.sub(r'Ã', '×', content)
-                content = re.sub(r'ï¬', 'fi', content)
-                content = re.sub(r'â', '∈', content)
-                content = re.sub(r'Â·', '·', content)
-                content = re.sub(r'ï¬', 'fl', content)
+                ##content = re.sub(r'â', '-', content)
+                ##content = re.sub(r'â', '∈', content)
+                ##content = re.sub(r'Ã', '×', content)
+                ##content = re.sub(r'ï¬', 'fi', content)
+                ##content = re.sub(r'â', '∈', content)
+                ##content = re.sub(r'Â·', '·', content)
+                ##content = re.sub(r'ï¬', 'fl', content)
     
                 pdf_url = f"{server_url}/{os.path.basename(metadata_dict['source'])}"
     
