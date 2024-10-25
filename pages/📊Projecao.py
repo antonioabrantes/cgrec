5aa94baab5da8d1cee83639722f9518f94bf82c4
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-10-25 14:39:47 -0300

    Update 📊Projecao.py

--- pages/📊Projecao.py
+++ pages/📊Projecao.py
@@ -345,18 +345,18 @@ def prompt_router(input):
         despacho = ''
         formatted_date = ''
         
-        if 'patents' in data and isinstance(data['patents'], list) and len(data['patents']) > 0 and 'descricao' in data['patents'][0]:
-            descricao = data['patents'][0]['descricao']
-        else:
-            descricao = None  # Ou alguma mensagem de erro ou tratamento apropriado
+        #if 'patents' in data and isinstance(data['patents'], list) and len(data['patents']) > 0 and 'descricao' in data['patents'][0]:
+        #    descricao = data['patents'][0]['descricao']
+        #else:
+        #    descricao = None  # Ou alguma mensagem de erro ou tratamento apropriado
     
-        #try:
-        #    if 'patents' in data and isinstance(data['patents'], list) and len(data['patents']) > 0 and 'descricao' in data['patents'][0]:
-        #        descricao = data['patents'][0]['descricao']
-        #    else:
-        #        descricao = ''
-        #except Exception as e:
-        #    descricao = 'nada'
+        try:
+            if 'patents' in data and isinstance(data['patents'], list) and len(data['patents']) > 0 and 'descricao' in data['patents'][0]:
+                descricao = data['patents'][0]['descricao']
+            else:
+                descricao = ''
+        except Exception as e:
+            descricao = 'nada'
 
         try:
             if 'patents' in data and isinstance(data['patents'], list) and len(data['patents']) > 0 and 'despacho' in data['patents'][0]:
