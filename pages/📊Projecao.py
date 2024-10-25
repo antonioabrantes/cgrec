f2b98a8e021222c2639fee17d86387766f527808
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-10-25 13:58:43 -0300

    Update 📊Projecao.py

--- pages/📊Projecao.py
+++ pages/📊Projecao.py
@@ -351,7 +351,7 @@ def prompt_router(input):
             despachos = [patent['despacho'] for patent in data['patents']]
             str_context = ''
             for despacho in despachos:
-                str_context = str_context + despacho
+                str_context = str_context + ' ' + despacho + ', '
             
             # http://www.cientistaspatentes.com.br/apiphp/patents/query/?q={%22mysql_query%22:%22*%20FROM%20revistas4%20WHERE%20numero=%27112021005834-6%27%20and%20data=%272024-10-22%27%20and%20despacho=%27PR%20-%20Recursos%27%22} 
             # http://www.cientistaspatentes.com.br/apiphp/patents/query/?q={"mysql_query":"* FROM revistas4 WHERE numero='112021005834-6' and data='2024-10-22' and despacho='PR - Recursos'"}
