eb3a637892c31b552ab14b38ea7c03fbdf32bf24
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-10-26 19:13:41 -0300

    Update 📚Analise.py

--- pages/📚Analise.py
+++ pages/📚Analise.py
@@ -1,8 +1,57 @@
 import streamlit as st
+import os, re
 import time
 import pandas as pd
+from langchain_openai import ChatOpenAI
+from langchain.prompts.prompt import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+import requests
+from datetime import datetime
+
+load_dotenv()
+#openai_api_key = os.environ['OPENAI_API_KEY']
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
 
 # https://emojipedia.org/books
+
+# Definindo cabeçalhos para a requisição
+headers = {
+    "Accept": "application/json",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+}
+
+def acessar_sinergias(url,headers):
+    try:
+        # Requisição para obter os dados JSON
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Verificar se a requisição foi bem-sucedida
+    
+        # Tentar decodificar o JSON
+        data = response.json()
+        #print(data)
+    
+        # Carregar os dados JSON em um DataFrame
+        #df = pd.DataFrame(data['despacho'])
+        #df['despacho'] = df['despacho'].fillna('Unknown')
+    
+        #print(df)
+    
+        # Verificar e converter a coluna 'count' para inteiro
+        #df['tempo'] = pd.to_numeric(df['tempo'], errors='coerce')
+        return data
+    
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except requests.exceptions.RequestException as req_err:
+        print(f"Error occurred during request: {req_err}")
+    except ValueError as json_err:
+        print(f"JSON decode error: {json_err}")
+    except Exception as err:
+        print(f"An unexpected error occurred: {err}")    
+    return -1
+
 
 def main():
     st.title("Análise de documentos de patente")
@@ -11,7 +60,8 @@ def main():
     st.write("Entre com o número do pedido de patente em recurso: p.ex. 102012005032")
     numero = st.text_input("Digite aqui:")
     if numero:
-        st.write("Numero: ",numero)
+        st.markdown("Numero: ",numero)
+        
 
 
 main()
