4587c5d940ecf68bf916400e2a615f961c755cad
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-10-26 22:02:48 -0300

    Update 📚Analise.py

--- pages/📚Analise.py
+++ pages/📚Analise.py
@@ -24,6 +24,78 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 }
 
+def summarize_the_pdf(
+    file_dir: str,
+    max_final_token: int,
+    token_threshold: int,
+    gpt_model: str,
+    temperature: float,
+    summarizer_llm_system_role: str,
+    final_summarizer_llm_system_role: str,
+    character_overlap: int
+):
+    """
+    Summarizes the content of a PDF file using OpenAI's ChatGPT engine.
+
+    Args:
+        file_dir (str): The path to the PDF file.
+        max_final_token (int): The maximum number of tokens in the final summary.
+        token_threshold (int): The threshold for token count reduction.
+        gpt_model (str): The ChatGPT engine model name.
+        temperature (float): The temperature parameter for ChatGPT response generation.
+        summarizer_llm_system_role (str): The system role for the summarizer.
+
+    Returns:
+        str: The final summarized content.
+    """
+    docs = []
+    docs.extend(PyPDFLoader(file_dir).load())
+    print(f"Document length: {len(docs)}")
+    max_summarizer_output_token = int(
+        max_final_token/len(docs)) - token_threshold
+    full_summary = ""
+    counter = 1
+    print("Generating the summary..")
+    # if the document has more than one pages
+    if len(docs) > 1:
+        for i in range(len(docs)):
+            # NOTE: This part can be optimized by considering a better technique for creating the prompt. (e.g: lanchain "chunksize" and "chunkoverlap" arguments.)
+
+            if i == 0:  # For the first page
+                prompt = docs[i].page_content + \
+                    docs[i+1].page_content[:character_overlap]
+            # For pages except the fist and the last one.
+            elif i < len(docs)-1:
+                prompt = docs[i-1].page_content[-character_overlap:] + \
+                    docs[i].page_content + \
+                    docs[i+1].page_content[:character_overlap]
+            else:  # For the last page
+                prompt = docs[i-1].page_content[-character_overlap:] + \
+                    docs[i].page_content
+            summarizer_llm_system_role = summarizer_llm_system_role.format(
+                max_summarizer_output_token)
+            full_summary += Summarizer.get_llm_response(
+                gpt_model,
+                temperature,
+                summarizer_llm_system_role,
+                prompt=prompt
+            )
+    else:  # if the document has only one page
+        full_summary = docs[0].page_content
+
+        print(f"Page {counter} was summarized. ", end="")
+        counter += 1
+    print("\nFull summary token length:", count_num_tokens(
+        full_summary, model=gpt_model))
+    final_summary = Summarizer.get_llm_response(
+        gpt_model,
+        temperature,
+        final_summarizer_llm_system_role,
+        prompt=full_summary
+    )
+    return final_summary
+
+
 def acessar_sinergias(url,headers):
     try:
         # Requisição para obter os dados JSON
@@ -85,6 +157,7 @@ def main():
             st.markdown(f"{kindcode} = {doc}")
 
             # https://patents.google.com/patent/US5866903A/en?oq=US5866903
+            # primeiro registro de anterioridades: 102012001686 WO2007057692 mas como teste use US20110314263
 
             url = f"https://patents.google.com/patent/{doc}A/en?oq={doc}"
             tentar_novamente = False
@@ -139,5 +212,7 @@ def main():
             query = f"Resuma o documento em português: {texto}"
             resposta = chain.invoke({"user_input":f"{query}"})
             st.markdown(f"Resumo {kindcode} {doc}: {resposta}")
+            
+            # sistema evista https://parecer.inpi.gov.br/patentes.php
 
 main()
