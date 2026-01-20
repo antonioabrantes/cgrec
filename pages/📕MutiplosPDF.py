bb7e228b09603a708a3be1dd71e76c67c4ff7114
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2026-01-20 19:12:58 -0300

    aa

--- pages/📕MutiplosPDF.py
+++ pages/📕MutiplosPDF.py
@@ -7,7 +7,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.embeddings import HuggingFaceInstructEmbeddings
 
-from langchain.vectorstores import FAISS
+#from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
