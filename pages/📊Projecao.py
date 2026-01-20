00ac0687e635147128c520fc26d0e59b78ed549f
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2026-01-20 20:12:52 -0300

    Update 📊Projecao.py

--- pages/📊Projecao.py
+++ pages/📊Projecao.py
@@ -5,7 +5,8 @@ import math
 import numpy as np
 import streamlit as st
 from langchain_openai import ChatOpenAI
-from langchain.prompts.prompt import PromptTemplate
+#from langchain.prompts.prompt import PromptTemplate
+from langchain.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 from langchain_core.runnables import RunnableLambda, RunnablePassthrough
