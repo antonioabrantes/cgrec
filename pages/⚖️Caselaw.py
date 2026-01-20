0e9fdac6d12134c799d0c20f802703ca4cc19766
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2026-01-20 20:48:58 -0300

    Update ⚖️Caselaw.py

--- pages/⚖️Caselaw.py
+++ pages/⚖️Caselaw.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
 
 #from langchain.prompts.prompt import PromptTemplate
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 # from langchain.chains.conversation.memory import ConversationBufferMemory
 #from langchain.text_splitter import RecursiveCharacterTextSplitter
 import tiktoken
