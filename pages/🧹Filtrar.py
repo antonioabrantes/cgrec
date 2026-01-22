6b3d1fe28a3eea508b21d0464100f98b3f589326
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2026-01-22 16:52:39 -0300

    Update 🧹Filtrar.py

--- pages/🧹Filtrar.py
+++ pages/🧹Filtrar.py
@@ -13,6 +13,7 @@ import io, os
 import requests
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate 
+from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 from langchain_core.messages import SystemMessage, HumanMessage
