1502be586071ca3f5c1a764c792e177a5fb59a5c
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2026-01-22 14:20:51 -0300

    Update 🙋Agentecgrec.py

--- pages/🙋Agentecgrec.py
+++ pages/🙋Agentecgrec.py
@@ -292,7 +292,8 @@ st.write(resposta)
 return_json = False
 resposta = ''
 headers = {
-    "User-Agent": "Mozilla/5.0"
+    "Accept": "application/json,text/plain;q=0.9",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 }
 response = requests.get(url,headers=headers,verify=False)
 if response.status_code == 200:
