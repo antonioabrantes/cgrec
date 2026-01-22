fe30c58e4bc46ec94dee5449ced83bee02617c7e
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2026-01-22 11:34:56 -0300

    Update 🙋Agentecgrec.py

--- pages/🙋Agentecgrec.py
+++ pages/🙋Agentecgrec.py
@@ -274,21 +274,23 @@ st.link_button(
    f"https://siscap.inpi.gov.br/adm/pareceres/{divisao}/00_{numero}{codigo}.pdf"
 )
 
-url = f"https://siscap.inpi.gov.br/adm/pareceres/{divisao}/00_{numero}{codigo}.pdf"
-PASTA_DOCS = "docs"
-#os.makedirs(PASTA_DOCS, exist_ok=True)
-nome_arquivo = f"00_{numero}{codigo}.pdf"
-#caminho_arquivo = os.path.join(PASTA_DOCS, nome_arquivo)
 headers = {
     "User-Agent": "Mozilla/5.0"
 }
+url = f"https://siscap.inpi.gov.br/adm/pareceres/{divisao}/{numero}{codigo}.txt"
+#PASTA_DOCS = "docs"
+#os.makedirs(PASTA_DOCS, exist_ok=True)
+#nome_arquivo = f"00_{numero}{codigo}.pdf"
+#caminho_arquivo = os.path.join(PASTA_DOCS, nome_arquivo)
 try:
     response = requests.get(url, headers=headers, verify=False, timeout=30)
     if response.status_code == 200:
+        texto = response.text  # 🔹 texto direto em memória sem ter feito download do arquivo
+        st.write(texto)
 #        with open(caminho_arquivo, "wb") as f:
 #            f.write(response.content)
-            st.success(f"Arquivo salvo localmente em: {caminho_arquivo}")
-            st.link_button("📄 Abrir parecer", caminho_arquivo)
+#            st.success(f"Arquivo salvo localmente em: {caminho_arquivo}")
+#            st.link_button("📄 Abrir parecer", caminho_arquivo)
     else:
         st.error("Não foi possível baixar o arquivo (status diferente de 200).")
 except Exception as e:
