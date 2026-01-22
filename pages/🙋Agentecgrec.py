30d2f48c8c509a64b89858511bd86ee3bbb19155
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2026-01-22 13:46:18 -0300

    Update 🙋Agentecgrec.py

--- pages/🙋Agentecgrec.py
+++ pages/🙋Agentecgrec.py
@@ -292,7 +292,7 @@ url = f"https://siscap.inpi.gov.br/adm/pareceres/{divisao}/{numero}{codigo}.txt"
 return_json = False
 resposta = ''
 headers = {
-    "Accept": "text/plain,application/json",
+    "Accept": "application/json,text/plain;q=0.9",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 }
 response = requests.get(url,headers=headers,verify=False)
